54f5326c8f6eb1d29ee250f90c6cb0e681e6a3f0
Freyness-gd <e12119042@student.tuwien.ac.at>
2025-11-07 23:08:33 +0100

    :wrench: feat: Add checkstyle config file

--- backend/src/main/java/at/ac/tuwien/mogda/willgraph/BackendApplication.java
+++ backend/src/main/java/at/ac/tuwien/mogda/willgraph/BackendApplication.java
@@ -6,7 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication {
 
-  public static void main(String[] args) {
+  private BackendApplication() {
+
+  }
+
+  static void main(final String[] args) {
     SpringApplication.run(BackendApplication.class, args);
   }
 
